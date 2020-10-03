3f19f07bd524a8d9a19ebeede09ea42ae5e72256
xxl4tomxu98 <tomxu@udel.edu>
2020-10-03 19:00:17 -0400

    original-seed

--- db/seeders/20201001154512-insert-shelves.js
+++ db/seeders/20201001154512-insert-shelves.js
@@ -4,9 +4,9 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Shelves', [
       { name: 'Favorites', userId: 2, createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Currently Reading', userId: 5, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Currently Reading', userId: 3, createdAt: new Date(), updatedAt: new Date() },
       { name: 'Book Club', userId: 1, createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Want to Read', userId: 4, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Want to Read', userId: 1, createdAt: new Date(), updatedAt: new Date() },
     ])
   },
 
