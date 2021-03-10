cd28a8ce4daa401f4083a4f35935395b6f2865ce
tom <tomxu@udel.edu>
2021-03-10 11:04:13 -0500

    add dialectOptions in database.js file for heroku deployment

--- config/database.js
+++ config/database.js
@@ -23,6 +23,12 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    },
     seederStorage: 'sequelize',
   },
 };
